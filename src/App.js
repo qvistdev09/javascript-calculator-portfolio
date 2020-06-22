0c2a20f5138e57b35f5f8c46aa2e1695c83c28a3
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-06-23 00:27:31 +0200

    update font, css fixes, hook up buttons with first draft of calc functions

--- src/App.js
+++ src/App.js
@@ -8,13 +8,55 @@ class App extends React.Component {
       gridItemHeight: 4,
       upperOutput: ' ',
       lowerOutput: '0',
+      decimals: false,
     };
     this.calcbuttonPress = this.calcbuttonPress.bind(this);
+    this.setZero = this.setZero.bind(this);
+    this.operatorPress = this.operatorPress.bind(this);
   }
 
   calcbuttonPress(btn) {
+    if (btn !== '.') {
+      this.setState((state) => {
+        if (state.lowerOutput === '0') {
+          return { lowerOutput: btn, upperOutput: btn };
+        } else {
+          return {
+            lowerOutput: state.lowerOutput + btn,
+            upperOutput: state.upperOutput + btn,
+          };
+        }
+      });
+    } else {
+      this.setState((state) => {
+        if (state.decimals === true) {
+          return;
+        }
+        if (state.lowerOutput === '0') {
+          return { lowerOutput: '0.', upperOutput: '0.', decimals: true };
+        } else {
+          return {
+            lowerOutput: state.lowerOutput + btn,
+            upperOutput: state.upperOutput + btn,
+            decimals: true,
+          };
+        }
+      });
+    }
+  }
+
+  setZero() {
+    this.setState({
+      lowerOutput: '0',
+      upperOutput: ' ',
+      decimals: false
+    });
+  }
+
+  operatorPress(operator) {
     this.setState((state) => ({
-      lowerOutput: state.lowerOutput + btn,
+      upperOutput: state.upperOutput + operator,
+      lowerOutput: operator
     }));
   }
 
@@ -31,23 +73,33 @@ class App extends React.Component {
           upper={this.state.upperOutput}
           lower={this.state.lowerOutput}
         />
-        <CalcButton label={'AC'} gridArea={'AC'} btnClass="AC" />
-        <CalcButton label={'/'} btnClass="light" />
-        <CalcButton label={'X'} btnClass="light" />
+        <CalcButton
+          label={'AC'}
+          gridArea={'AC'}
+          btnClass="AC"
+          press={this.setZero}
+        />
+        <CalcButton label={'/'} btnClass="light" press={this.operatorPress}/>
+        <CalcButton label={'*'} btnClass="light" press={this.operatorPress}/>
         <CalcButton label={'7'} btnClass="dark" press={this.calcbuttonPress} />
-        <CalcButton label={'8'} btnClass="dark" />
-        <CalcButton label={'9'} btnClass="dark" />
-        <CalcButton label={'-'} btnClass="light" />
-        <CalcButton label={'4'} btnClass="dark" />
-        <CalcButton label={'5'} btnClass="dark" />
-        <CalcButton label={'6'} btnClass="dark" />
-        <CalcButton label={'+'} btnClass="light" />
-        <CalcButton label={'1'} btnClass="dark" />
-        <CalcButton label={'2'} btnClass="dark" />
-        <CalcButton label={'3'} btnClass="dark" />
+        <CalcButton label={'8'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'9'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'-'} btnClass="light" press={this.operatorPress}/>
+        <CalcButton label={'4'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'5'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'6'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'+'} btnClass="light" press={this.operatorPress}/>
+        <CalcButton label={'1'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'2'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'3'} btnClass="dark" press={this.calcbuttonPress} />
         <CalcButton label={'='} gridArea="equal" btnClass="equal" />
-        <CalcButton label={'0'} gridArea="zero" btnClass="dark" />
-        <CalcButton label={'.'} btnClass="dark" />
+        <CalcButton
+          label={'0'}
+          gridArea="zero"
+          btnClass="dark"
+          press={this.calcbuttonPress}
+        />
+        <CalcButton label={'.'} btnClass="dark" press={this.calcbuttonPress} />
       </div>
     );
   }
