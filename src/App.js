e2d0f19725fc4651cc65dc3e212b1344f2eee599
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-06-22 21:12:29 +0200

    first css pass, initial button functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridItemWidth: 4,
+      gridItemHeight: 4,
+      upperOutput: ' ',
+      lowerOutput: '0',
+    };
+    this.calcbuttonPress = this.calcbuttonPress.bind(this);
+  }
+
+  calcbuttonPress(btn) {
+    this.setState((state) => ({
+      lowerOutput: state.lowerOutput + btn,
+    }));
+  }
+
+  render() {
+    return (
+      <div
+        id="calculator"
+        style={{
+          gridTemplateColumns: 'repeat(4, ' + this.state.gridItemWidth + 'rem)',
+          gridTemplateRows: 'repeat(6, ' + this.state.gridItemHeight + 'rem)',
+        }}
+      >
+        <OutputField
+          upper={this.state.upperOutput}
+          lower={this.state.lowerOutput}
+        />
+        <CalcButton label={'AC'} gridArea={'AC'} btnClass="AC" />
+        <CalcButton label={'/'} btnClass="light" />
+        <CalcButton label={'X'} btnClass="light" />
+        <CalcButton label={'7'} btnClass="dark" press={this.calcbuttonPress} />
+        <CalcButton label={'8'} btnClass="dark" />
+        <CalcButton label={'9'} btnClass="dark" />
+        <CalcButton label={'-'} btnClass="light" />
+        <CalcButton label={'4'} btnClass="dark" />
+        <CalcButton label={'5'} btnClass="dark" />
+        <CalcButton label={'6'} btnClass="dark" />
+        <CalcButton label={'+'} btnClass="light" />
+        <CalcButton label={'1'} btnClass="dark" />
+        <CalcButton label={'2'} btnClass="dark" />
+        <CalcButton label={'3'} btnClass="dark" />
+        <CalcButton label={'='} gridArea="equal" btnClass="equal" />
+        <CalcButton label={'0'} gridArea="zero" btnClass="dark" />
+        <CalcButton label={'.'} btnClass="dark" />
+      </div>
+    );
+  }
+}
+
+function CalcButton(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={props.btnClass ? props.btnClass + ' calc-child' : 'calc-child'}
+      style={
+        props.gridArea ? { gridArea: props.gridArea } : { gridArea: 'initial' }
+      }
+      onClick={() => props.press(props.label)}
+    >
+      {props.label}
+    </div>
+  );
+}
+
+function OutputField(props) {
+  return (
+    <div className="calc-child output" style={{ gridArea: 'output' }}>
+      <div>{props.upper}</div>
+      <div>{props.lower}</div>
     </div>
   );
 }
